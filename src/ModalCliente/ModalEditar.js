4cbb32413da4c5df8a8c27fe5f68a22405c0b807
jessicaduarte95 <jessicaduarte.95@gmail.com>
2023-03-18 17:07:15 -0300

    Ajuste - Editar Cliente

--- src/ModalCliente/ModalEditar.js
+++ src/ModalCliente/ModalEditar.js
@@ -26,58 +26,56 @@ export const ModalEditar = (props) => {
     };
 
   return (
-    <div>
-      <Modal
-        open={openEditarCliente}
-        onClose={handleCloseEditarCliente}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2" style={{color: "#424242", fontFamily: 'Arial', fontWeight: "bold", fontSize: '1.1rem',  marginBottom: "1rem"}}>
-            Editar Cliente
-          </Typography>
-          <form id="editar" onSubmit={handleSubmit(onSubmit)}>
-            <Grid>
-                <Box
+    <Modal
+    open={openEditarCliente}
+    onClose={handleCloseEditarCliente}
+    aria-labelledby="modal-modal-title"
+    aria-describedby="modal-modal-description"
+    >
+    <Box sx={style}>
+        <Typography id="modal-modal-title" variant="h6" component="h2" style={{color: "#424242", fontFamily: 'Arial', fontWeight: "bold", fontSize: '1.1rem',  marginBottom: "1rem"}}>
+        Editar Cliente
+        </Typography>
+        <form id="editar" onSubmit={handleSubmit(onSubmit)}>
+        <Grid>
+            <Box
+            component="form"
+            sx={{
+                '& > :not(style)': { m: 0.3 }
+            }}
+            noValidate
+            autoComplete="off"
+            >
+                <TextField id="nome" label="Nome" variant="outlined" {...register("nome", { required: true })} style={{width: "100%", marginBottom: "1rem"}}/>
+
+            </Box>
+            <Box
                 component="form"
                 sx={{
-                  '& > :not(style)': { m: 0.3 }
+                '& > :not(style)': { m: 0.3},
                 }}
                 noValidate
                 autoComplete="off"
                 >
-                  <TextField id="nome" label="Nome" variant="outlined" {...register("nome", { required: true })} style={{width: "100%", marginBottom: "1rem"}}/>
-
-                </Box>
-                <Box
-                  component="form"
-                  sx={{
-                    '& > :not(style)': { m: 0.3},
-                  }}
-                  noValidate
-                  autoComplete="off"
-                  >
-                  <TextField id="endereco" label="Endereço" variant="outlined" {...register("endereco", { required: true })} style={{width: "100%", marginBottom: "1rem"}}/>
-                </Box>
-                <Box
-                  component="form"
-                  sx={{
-                    '& > :not(style)': { m: 0.3},
-                  }}
-                  noValidate
-                  autoComplete="off"
-                  >
-                  <TextField id="cpf" label="CPF" variant="outlined"  {...register("cpf", { required: true })} style={{width: "100%"}}/>
-                </Box>
-            </Grid>
-          </form>
-          <Grid container item sm={12} style={{marginTop: "1rem", justifyContent: "flex-end"}}>
-            <Button type="submit" variant="outlined" style={{height: '2.5rem', width: "6.5rem"}} onClick={handleCloseEditarCliente}>Cancelar</Button>
-            <Button type="submit" variant="contained" style={{height: '2.5rem', width: "5.5rem", marginLeft: "0.7rem"}}>Salvar</Button>
-         </Grid>
-        </Box>
-      </Modal>
-    </div>
+                <TextField id="endereco" label="Endereço" variant="outlined" {...register("endereco", { required: true })} style={{width: "100%", marginBottom: "1rem"}}/>
+            </Box>
+            <Box
+                component="form"
+                sx={{
+                '& > :not(style)': { m: 0.3},
+                }}
+                noValidate
+                autoComplete="off"
+                >
+                <TextField id="cpf" label="CPF" variant="outlined"  {...register("cpf", { required: true })} style={{width: "100%"}}/>
+            </Box>
+        </Grid>
+        </form>
+        <Grid container item sm={12} style={{marginTop: "1rem", justifyContent: "flex-end"}}>
+        <Button type="submit" variant="outlined" style={{height: '2.5rem', width: "6.5rem"}} onClick={handleCloseEditarCliente}>Cancelar</Button>
+        <Button type="submit" variant="contained" style={{height: '2.5rem', width: "5.5rem", marginLeft: "0.7rem"}}>Salvar</Button>
+        </Grid>
+    </Box>
+    </Modal>
   );
 }
